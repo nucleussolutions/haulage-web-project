fc89e8a8a3f866898304338b615d0a6fc37370b1
kevintan <kevintanhongann@gmail.com>
2017-08-06 19:33:23 +0800

    first commit

---
+++ server/grails-app/controllers/haulage/angular/profile/ApplicationController.groovy
@@ -0,0 +1,15 @@
+package haulage.angular.profile
+
+import grails.core.GrailsApplication
+import grails.util.Environment
+import grails.plugins.*
+
+class ApplicationController implements PluginManagerAware {
+
+    GrailsApplication grailsApplication
+    GrailsPluginManager pluginManager
+
+    def index() {
+        [grailsApplication: grailsApplication, pluginManager: pluginManager]
+    }
+}
---
+++ server/grails-app/init/haulage/angular/profile/BootStrap.groovy
@@ -0,0 +1,9 @@
+package haulage.angular.profile
+
+class BootStrap {
+
+    def init = { servletContext ->
+    }
+    def destroy = {
+    }
+}
