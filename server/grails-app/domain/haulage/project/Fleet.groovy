aaacdab31ac6856ad6e9f87fe87a8aaec6d0fe97
kevintan <kevintanhongann@gmail.com>
2017-09-11 10:27:42 +0800

    fleet with validators

--- server/grails-app/domain/haulage/project/Fleet.groovy
+++ server/grails-app/domain/haulage/project/Fleet.groovy
@@ -5,6 +5,20 @@ import grails.rest.*
 
 @Resource(readOnly = false, formats = ['json', 'xml'])
 class Fleet {
-    Vehicle primeMover;
-    Vehicle trailer;
+
+    Vehicle primeMover
+    Vehicle trailer
+
+    static constraints = {
+        primeMover nullable: false, validator: {
+            if(primeMover.type == 'trailer'){
+                return 'type cannot be trailer'
+            }
+        }
+        trailer nullable: false, validator: {
+            if(trailer.type == 'primeMover'){
+                return 'type cannot be prime mover'
+            }
+        }
+    }
 }
