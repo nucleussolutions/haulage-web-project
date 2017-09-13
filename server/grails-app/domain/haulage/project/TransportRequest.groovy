ce32b452cb94829b5a04e709408fec21e2d4d63b
kevintan <kevintanhongann@gmail.com>
2017-09-13 11:37:32 +0800

    wip embedded customer info inside rft

--- server/grails-app/domain/haulage/project/TransportRequest.groovy
+++ server/grails-app/domain/haulage/project/TransportRequest.groovy
@@ -87,7 +87,20 @@ class TransportRequest {
         customer nullable: false
     }
 
+    static embedded = ['customer']
+
     static mapping = {
         autoTimestamp true
     }
+}
+
+class Customer {
+    String companyName
+    String personInCharge
+    String phone
+    String address1
+    String address2
+    String city
+    String state
+    String country
 }
