5d1b5bb4106fadd3743c52fd358094c82f1c5ddd
kevintan <kevintanhongann@gmail.com>
2017-11-13 11:14:01 +0800

    generate pdf stub

---
+++ server/grails-app/services/haulage/project/PdfGenerationService.groovy
@@ -0,0 +1,39 @@
+package haulage.project
+
+import org.grails.plugins.wkhtmltopdf.WkhtmltoxService
+
+class PdfGenerationService {
+
+  static transactional = false
+
+  WkhtmltoxService wkhtmltoxService
+
+  def generateQuotation(Quote quote){
+    //todo generate quote
+
+  }
+
+  def generateConsignmentNote(Consignment consignment){
+    //todo generate consignment
+    def byte[] pdfData = wkhtmltoxService.makePdf(
+            view: "/pdf/someGspTemplate",
+            model: [someInstance: someInstance],
+            header: "/pdf/someHeader",
+            footer: "/pdf/someFooter",
+            marginLeft: 20,
+            marginTop: 35,
+            marginBottom: 20,
+            marginRight: 20,
+            headerSpacing: 10)
+
+
+    // DO Something e.g. send as mail
+    //sendAsynchronousMail {
+    //    multipart true
+    //    to "mail@mail.de"
+    //    subject "see PDF Attachment";
+    //    attachBytes "PDF Attachment.pdf", "application/pdf", pdfData
+    //    body "see my pdf attachment"
+    //}
+  }
+}
