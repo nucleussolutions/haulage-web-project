65139e6b22d5e42c5d738b5c4fdef3ba0d770ff5
kevintan <kevintanhongann@gmail.com>
2017-12-07 12:01:26 +0800

    wip

--- client/src/app/reporting/reporting.component.ts
+++ client/src/app/reporting/reporting.component.ts
@@ -1,11 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-reporting',
   templateUrl: './reporting.component.html',
   styleUrls: ['./reporting.component.css']
 })
-export class ReportingComponent implements OnInit {
+export class ReportingComponent implements OnInit, OnDestroy {
+  ngOnDestroy(): void {
+  }
 
   constructor() { }
 
