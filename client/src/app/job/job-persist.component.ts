209ee8af547fe8539e0ddd2487512ae59ab559b1
kevintan <kevintanhongann@gmail.com>
2018-01-25 02:54:21 +0800

    wip

--- client/src/app/job/job-persist.component.ts
+++ client/src/app/job/job-persist.component.ts
@@ -1,67 +1,77 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { ActivatedRoute, Params, Router } from '@angular/router';
-import { Job } from './job';
-import { JobService } from './job.service';
-import { ConsignmentService } from '../consignment/consignment.service';
-import { Consignment } from '../consignment/consignment';
-import { Subscription } from "rxjs/Subscription";
-import { UserService } from "../user.service";
-import { PermissionService } from "../permission/permission.service";
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {ActivatedRoute, Params, Router} from '@angular/router';
+import {Job} from './job';
+import {JobService} from './job.service';
+import {ConsignmentService} from '../consignment/consignment.service';
+import {Consignment} from '../consignment/consignment';
+import {Subscription} from "rxjs/Subscription";
+import {UserService} from "../user.service";
+import {PermissionService} from "../permission/permission.service";
+import {Permission} from "../permission/permission";
+import {Observable} from "rxjs/Observable";
 
 @Component({
-    selector: 'job-persist',
-    templateUrl: './job-persist.component.html',
-    providers: [UserService]
+  selector: 'job-persist',
+  templateUrl: './job-persist.component.html',
+  providers: [UserService]
 })
 export class JobPersistComponent implements OnInit, OnDestroy {
 
-    ngOnDestroy(): void {
-        if(this.subscription){
-            this.subscription.unsubscribe();
-        }
+  ngOnDestroy(): void {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
     }
+  }
 
-    job = new Job();
-    create = true;
-    errors: any[];
-    consignmentList: Consignment[];
+  job = new Job();
+  create = true;
+  errors: any[];
+  consignmentList: Consignment[];
 
-    private subscription: Subscription;
+  private subscription: Subscription;
 
-    private userObject: any;
+  private userObject: any;
 
-    // tslint:disable-next-line:max-line-length
-    constructor(private route: ActivatedRoute, private jobService: JobService, private router: Router, private consignmentService: ConsignmentService, private userService: UserService, private permissionService: PermissionService) {
-    }
+  private permission: Permission;
 
-    ngOnInit() {
-        this.subscription = this.userService.getUser().subscribe(userObject => {
-            this.userObject = userObject;
+  // tslint:disable-next-line:max-line-length
+  constructor(private route: ActivatedRoute, private jobService: JobService, private router: Router, private consignmentService: ConsignmentService, private userService: UserService, private permissionService: PermissionService) {
+  }
+
+  ngOnInit() {
+
+    this.subscription = this.userService.getUser().flatMap(userObject => {
+      this.userObject = userObject;
+      return this.permissionService.getByUserId(this.userObject);
+    }).subscribe(permission => {
+      this.permission = permission;
+    });
+
+    this.route.params.subscribe((params: Params) => {
+      if (params.hasOwnProperty('id')) {
+        this.jobService.get(+params['id'], this.userObject).subscribe((job: Job) => {
+          this.create = false;
+          this.job = job;
         });
+      }
+    });
 
-        this.consignmentService.list(this.userObject, 1).subscribe((consignmentList: Consignment[]) => {
-            this.consignmentList = consignmentList;
-        });
-        this.route.params.subscribe((params: Params) => {
-            if (params.hasOwnProperty('id')) {
-                this.jobService.get(+params['id'], this.userObject).subscribe((job: Job) => {
-                    this.create = false;
-                    this.job = job;
-                });
-            }
-        });
-    }
+    this.consignmentService.list(this.userObject, 1).subscribe((consignmentList: Consignment[]) => {
+      this.consignmentList = consignmentList;
+    });
 
-    save() {
-        this.jobService.save(this.job, this.userObject).subscribe((job: Job) => {
-            this.router.navigate(['/job', 'show', job.id]);
-        }, (res: Response) => {
-            const json = res.json();
-            if (json.hasOwnProperty('message')) {
-                this.errors = [json];
-            } else {
-                // this.errors = json._embedded.errors;
-            }
-        });
-    }
+  }
+
+  save() {
+    this.jobService.save(this.job, this.userObject).subscribe((job: Job) => {
+      this.router.navigate(['/job', 'show', job.id]);
+    }, (res: Response) => {
+      const json = res.json();
+      if (json.hasOwnProperty('message')) {
+        this.errors = [json];
+      } else {
+        // this.errors = json._embedded.errors;
+      }
+    });
+  }
 }
