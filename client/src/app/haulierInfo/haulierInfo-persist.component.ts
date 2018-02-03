4ff8cde97a6cf3d3fc93109c17793fff6a7f9cf9
kevintan <kevintanhongann@gmail.com>
2018-02-03 14:07:07 +0800

    make search company available

--- client/src/app/haulierInfo/haulierInfo-persist.component.ts
+++ client/src/app/haulierInfo/haulierInfo-persist.component.ts
@@ -7,6 +7,7 @@ import { CompanyService } from '../company/company.service';
 import { Company } from '../company/company';
 import { Subscription } from 'rxjs/Subscription';
 import { UserService } from 'app/user.service';
+import {Observable} from "rxjs/Observable";
 
 
 @Component({
@@ -23,6 +24,23 @@ export class HaulierInfoPersistComponent implements OnInit {
   private subscription: Subscription;
 
   private userObject: any;
+
+  companySearch = (text$: Observable<string>) =>
+      text$
+          .debounceTime(200)
+          .distinctUntilChanged()
+          .switchMap(term => term.length < 2 && this.userObject   // switch to new observable each time
+              // return the http search observable
+              ? [] : this.companyService.search(term, this.userObject)
+              // or the observable of empty heroes if no search term
+                  .map(json => {
+                    this.companyList = json['searchResults'];
+                    if(this.companyList){
+                      return json['searchResults'].map(item => item.name);
+                    }else{
+                      throw 'not found';
+                    }
+                  }));
 
   constructor(private route: ActivatedRoute, private haulierInfoService: HaulierInfoService, private router: Router, private companyService: CompanyService, private userService: UserService) {
     this.subscription = this.userService.getUser().subscribe(response => {
