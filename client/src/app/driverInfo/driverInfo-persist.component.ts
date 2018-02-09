641dbcfcd14a5d7585f8bd4d79ed3ddab034ead5
kevintan <kevintanhongann@gmail.com>
2018-02-09 10:23:15 +0800

    cleanup

--- client/src/app/driverInfo/driverInfo-persist.component.ts
+++ client/src/app/driverInfo/driverInfo-persist.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params, Router } from '@angular/router';
-import { DriverInfo } from './driverInfo';
-import { DriverInfoService } from './driverInfo.service';
-import { UserService } from 'app/user.service';
-import { Subscription } from 'rxjs/Subscription';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Params, Router} from '@angular/router';
+import {DriverInfo} from './driverInfo';
+import {DriverInfoService} from './driverInfo.service';
+import {UserService} from 'app/user.service';
+import {Subscription} from 'rxjs/Subscription';
+import {Observable} from "rxjs/Observable";
 
 
 @Component({
@@ -18,36 +19,43 @@ export class DriverInfoPersistComponent implements OnInit {
 
   private subscription: Subscription;
 
-  private userObject : any;
+  private userObject: any;
 
   constructor(private route: ActivatedRoute, private driverInfoService: DriverInfoService, private router: Router, private userService: UserService) {
-    this.subscription = this.userService.getUser().subscribe(userObject => {
-      this.userObject = userObject;
-    });
+
 
   }
 
   ngOnInit() {
-    this.route.params.subscribe((params: Params) => {
+    this.subscription = Observable.combineLatest(this.userService.getUser(), this.route.params).flatMap(result => {
+      this.userObject = result[0];
+
+      let params = result[1];
+
       if (params.hasOwnProperty('id')) {
-        this.driverInfoService.get(+params['id'], this.userObject).subscribe((driverInfo: DriverInfo) => {
-          this.create = false;
-          this.driverInfo = driverInfo;
-        });
+        return this.driverInfoService.get(+params['id'], this.userObject);
+      }else{
+        throw 'params id not found, nothing to see here';
       }
+    }).subscribe((driverInfo: DriverInfo) => {
+      this.create = false;
+      this.driverInfo = driverInfo;
     });
   }
 
   save() {
     this.driverInfoService.save(this.driverInfo, this.userObject).subscribe((driverInfo: DriverInfo) => {
       this.router.navigate(['/driverInfo', 'show', driverInfo.id]);
-    }, (res: Response) => {
-      const json = res.json();
+    }, (json) => {
+      console.log('json error ' + JSON.stringify(json));
       if (json.hasOwnProperty('message')) {
-        this.errors = [json];
+        this.errors = json.error._embedded.errors;
+        console.info('[json.error]');
       } else {
-        // this.errors = json._embedded.errors;
+        this.errors = json._embedded.errors;
+        console.info('json._embedded.errors');
       }
+      console.log('this.errors ' + JSON.stringify(this.errors));
     });
   }
 }
