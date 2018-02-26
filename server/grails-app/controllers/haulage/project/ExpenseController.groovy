e07b21c5a11edf5210fa42eff65ee4c96af9ea41
kevintan <kevintanhongann@gmail.com>
2018-02-26 09:19:42 +0800

    tripsheet by haulier id, otherwise show all as super admin

--- server/grails-app/controllers/haulage/project/ExpenseController.groovy
+++ server/grails-app/controllers/haulage/project/ExpenseController.groovy
@@ -3,6 +3,7 @@ package haulage.project
 
 import grails.rest.*
 import grails.converters.*
+import org.springframework.http.HttpStatus
 
 class ExpenseController extends RestfulController {
 
@@ -11,5 +12,36 @@ class ExpenseController extends RestfulController {
   ExpenseController() {
     super(Expense)
   }
+
+  @Override
+  def index(Integer max) {
+    //todo get permission of user
+    def userId = request.getHeader('userId')
+    if(userId){
+      Permission permission = Permission.where {
+        userInfo.userId == userId
+      }
+
+      if(permission){
+        if(permission.authority == 'Super Admin'){
+          //show all
+          return super.index(max)
+        }else{
+          //todo show expenses based on haulier id
+          def expenses = Expense.where {
+            job.haulierId == userId
+          }
+          return expenses
+        }
+      }else{
+        response.status = HttpStatus.BAD_REQUEST.value()
+        respond message: 'permission not found'
+      }
+    }else{
+      response.status = HttpStatus.NOT_FOUND.value()
+      respond message: 'user id not found'
+    }
+  }
+
 
 }
