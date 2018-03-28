e3eb31c82489e40b9002d725482b10820729d3fa
kevintan <kevintanhongann@gmail.com>
2018-03-28 11:10:08 +0800

    fix phone validator

--- client/src/app/validators/malaysian-phone-validator.ts
+++ client/src/app/validators/malaysian-phone-validator.ts
@@ -1,18 +1,19 @@
-import {AbstractControl, ValidationErrors, ValidatorFn} from "@angular/forms";
+import {AbstractControl, AsyncValidatorFn, ValidationErrors, ValidatorFn} from "@angular/forms";
 import {Observable} from "rxjs/Observable";
 import {isValidNumber} from "libphonenumber-js";
 import {Subject} from "rxjs/Subject";
 
-export function malaysianPhoneNumberValidator(): ValidatorFn{
+export function malaysianPhoneNumberValidator(): AsyncValidatorFn{
   return (control: AbstractControl): Promise<ValidationErrors | null> | Observable<ValidationErrors | null> => {
     let subject = new Subject<any>();
     let valid = isValidNumber(control.value, 'MY');
+    console.log('isValidNumber '+valid);
     if(valid){
       console.log('valid phone number');
       subject.next(null);
     }else{
       console.log('invalid phone number');
-      subject.error('not valid');
+      subject.next('not valid');
     }
     return subject.asObservable();
   }
