46f5a4f572924d61acedea19cee8ce01c78af48f
kevintan <kevintanhongann@gmail.com>
2018-04-05 16:57:03 +0800

    domain class constraints unit test

--- server/src/test/groovy/haulage/project/CompanySpec.groovy
+++ server/src/test/groovy/haulage/project/CompanySpec.groovy
@@ -11,34 +11,66 @@ class CompanySpec extends Specification implements DomainUnitTest<Company> {
     def cleanup() {
     }
 
-    void "company save successful"() {
+    void "test name cannot be null"(){
 
-        given: 'company information is filled'
-        def company = new Company()
-        company.name = 'AGL Logistics'
-        company.email = 'kevin@kevin.com'
-        company.address1 = 'test address 1'
-        company.address2 = 'test address 2'
-        company.city = 'Petaling Jaya'
-        company.state = 'Selangor'
-        company.country = 'Malaysia'
-
-        when: 'permissions are defined'
-        def permission = new Permission()
-        permission.company = company
-        permission.role = 'Owner'
-        permission.authority = 'Admin'
-        permission.status = 'Pending Approval'
-
-        company.permissions = []
-        company.permissions.add(permission)
-
-
-        then: 'company save successful, with permissions'
-        company.save()
-        assert company.permissions != null
-
-//        expect:"fix me"
-//        true == false
     }
+
+    void "test name cannot be blank"(){
+
+    }
+
+    void "test registrationNo cannot be null"(){
+
+    }
+
+    void "test registrationNo has to be unique"(){
+
+    }
+
+    void "test address1 cannot be null"(){
+
+    }
+
+    void "test address2 cannot be null"(){
+
+    }
+
+    void "test city cannot be null"(){
+
+
+    }
+
+    void "test state cannot be null"(){
+
+    }
+
+    void "test country cannot be null"(){
+
+    }
+
+    void "test officePhone cannot be null"(){
+
+    }
+
+    void "test officePhone cannot be blank"(){
+
+    }
+
+    void "test code cannot be null"(){
+
+    }
+
+    void "test code must be unique"(){
+
+    }
+
+    void "test email cannot be null"(){
+
+    }
+
+    void "test email must be email"(){
+
+    }
+
+
 }
